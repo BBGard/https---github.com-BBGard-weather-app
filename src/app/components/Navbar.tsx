7eaab1c1726edbd76094d130ac0c7048b05991ab
bbgard <gonzogardiner@gmail.com>
2024-02-06 11:03:07 +1100

    initial commit

---
+++ src/app/components/Navbar.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { FaSun } from "react-icons/fa";
+import { FaLocationCrosshairs } from "react-icons/fa6";
+import { FaLocationDot } from "react-icons/fa6";
+
+
+
+type Props = {};
+
+export default function Navbar(props: Props) {
+  return (
+    <nav className="shadow-sm sticky top-0 left-0 z-50 bg-white">
+      <div className="h-[80px] w-full flex justify-between items-center max-w-7xl px-3 mx-auto">
+        <p className="flex items-center justify-center gap-2">
+          <h2 className="text-gray-500 text-3xl">Weather</h2>
+          <FaSun className="text-yellow-300 text-3xl" />
+        </p>
+
+        <section className="flex gap-2 items-center">
+          <FaLocationCrosshairs className="text-gray-400 text-2xl hover:opacity-80 cursor-pointer" />
+          <FaLocationDot className="text-gray-400 text-2xl hover:opacity-80 cursor-pointer" />
+          <p className="text-slate-900/80 text-sm">
+            Melbourne
+          </p>
+          <div className="w-8 h-8 bg-gray-200 rounded-full">
+            {/* {SearchBox} */}
+          </div>
+          </section>
+
+      </div>
+    </nav>
+  );
+}
